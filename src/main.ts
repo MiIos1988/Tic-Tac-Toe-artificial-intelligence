ff685e887923faf9a49f73cbdde2f2c3fc0b8720
Milos1988 <vojvoda1988@gmail.com>
2023-12-10 21:30:46 +0100

    Set typescript

--- src/main.ts
+++ src/main.ts
@@ -2786,20 +2786,6 @@ function valueMatrix() {
   ) {
     if (matrix[1][2] === 0) matrix[1][2] = 2;
   }
-  //Game over
-  // if (
-  //   matrix[0][0] !== 0 &&
-  //   matrix[0][1] !== 0 &&
-  //   matrix[0][2] !== 0 &&
-  //   matrix[1][0] !== 0 &&
-  //   matrix[1][1] !== 0 &&
-  //   matrix[1][2] !== 0 &&
-  //   matrix[2][0] !== 0 &&
-  //   matrix[2][1] !== 0 &&
-  //   matrix[2][2] !== 0
-  // ) {
-  //   text2.innerText = "GAME OVER";
-  // }
   if (
     (matrix[0][0] === 1 && matrix[0][1] === 1 && matrix[0][2] === 1) ||
     (matrix[1][0] === 1 && matrix[1][1] === 1 && matrix[1][2] === 1) ||
